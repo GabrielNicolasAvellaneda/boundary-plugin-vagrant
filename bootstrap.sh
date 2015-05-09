5dc66652a535c6fa7fe5f53ce157492eccb3baa7
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-05-09 16:55:17 +0000

    bootstrap.sh can be called with an optional argument that specifies the application to configure. If no argument are specified it will configure all applications and services.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,39 @@
 
 GIT_USER_NAME="Your Git Username"
 GIT_USER_EMAIL="your@email.com"
+GIT_CORE_EDITOR=vim
+
+BOUNDARY_API_TOKEN="api.4a188c9650-6506"
 
 # Basic system configuration
-sudo apt-get install -y git
-git config --global user.name $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
-git config --global core.editor vim
+
+if [[ $# -eq 0 || $1 = 'git' ]]
+then
+  sudo apt-get install -y git
+  git config --global user.name $GIT_USER_NAME
+  git config --global user.email $GIT_USER_EMAIL
+  git config --global core.editor $GIT_CORE_EDITOR
+fi
+
+# Boundary meter installation
+if [[ $# -eq 0 || $1 = "boundary" ]]
+then
+  curl -fsS \
+    -d "{\"token\":\"$BOUNDARY_API_TOKEN\"}" \
+    -H "Content-Type: application/json" \
+    "https://meter.boundary.com/setup_meter" > setup_meter.sh
+  chmod +x setup_meter.sh
+  ./setup_meter.sh
+  rm setup_meter.sh
+fi
 
 # Luachech installation 
-sudo apt-get update
-sudo apt-get install -y luarocks
-sudo luarocks install luacheck
+if [[ $# -eq 0 || $1 = "luacheck" ]]
+then
+  sudo apt-get update
+  sudo apt-get install -y luarocks
+  sudo luarocks install luacheck
+fi
 
 # HAProxy
 # TODO: Install latest verions of HAProxy for v1.3.x, v1.4.x, v1.5.x, v1.6.x
